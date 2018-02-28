5586e4a96e35642a3751f7439ff65cc08fe9c2cf
sbx_user1059 <sbx_user1059@ip-10-12-138-233.eu-west-2.compute.internal>
2018-02-28 12:59:22 +0000

    Wed Feb 28 12:59:22 UTC 2018

--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -11,4 +11,14 @@ public class SumTest {
     public void compute_sum() {
         assertThat(Sum.sum(1, 1), equalTo(2));
     }
+
+    @Test
+    public void compute_sum_large() {
+        assertThat(Sum.sum(100, 100), equalTo(200));
+    }
+
+    @Test
+    public void compute_sum_zero() {
+        assertThat(Sum.sum(0, 0), equalTo(0));
+    }
 }
