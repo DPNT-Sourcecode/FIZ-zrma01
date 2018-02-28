d866254553c6847071c93cbc585d5995de9280eb
sbx_user1059 <sbx_user1059@ip-10-12-138-233.eu-west-2.compute.internal>
2018-02-28 13:08:43 +0000

    Wed Feb 28 13:08:43 UTC 2018

--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -1,4 +1,29 @@
 package befaster.solutions;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void number() {
+        assertThat(FizzBuzz.fizzBuzz(1), equalTo("1"));
+    }
+
+    @Test
+    public void fizz() {
+        assertThat(FizzBuzz.fizzBuzz(3), equalTo("fizz"));
+    }
+
+    @Test
+    public void buzz() {
+        assertThat(FizzBuzz.fizzBuzz(5), equalTo("buzz"));
+    }
+
+    @Test
+    public void fizzBuzz() {
+        assertThat(FizzBuzz.fizzBuzz(15), equalTo("fizz buzz"));
+    }
 }
