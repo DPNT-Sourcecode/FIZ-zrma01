ff84f94e15ac3bf839d0097525b1d20e1750bfbd
sbx_user1060 <sbx_user1060@ip-10-12-239-173.eu-west-2.compute.internal>
2018-02-28 12:47:57 +0000

    Wed Feb 28 12:47:57 UTC 2018

--- src/main/java/befaster/runner/ConfigNotFoundException.java
+++ src/main/java/befaster/runner/ConfigNotFoundException.java
@@ -1,7 +1,5 @@
 package befaster.runner;
 
-import com.sun.tools.internal.ws.wsdl.document.jaxws.Exception;
-
 public class ConfigNotFoundException extends Exception {
     ConfigNotFoundException(String message) {
         super(message);
